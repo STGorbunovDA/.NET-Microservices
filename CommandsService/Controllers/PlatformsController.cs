9bb187f375a85fb4657e7f8ff82142f1554b92f9
Дмитрий Горбунов <bous07@mail.ru>
2023-11-14 14:08:42 +0800

    Add CommandsService

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController() { }
+
+        //public PlatformsController(ICommandRepo repository, IMapper mapper) => (_repository, mapper) = (repository, mapper);
+
+
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
+        }
+    }
+}
