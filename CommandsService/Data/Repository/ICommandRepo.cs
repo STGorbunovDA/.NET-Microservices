d65cd25bf4901b03ab65f27d549515b13765ce74
Дмитрий Горбунов <bous07@mail.ru>
2023-11-15 20:24:59 +0800

    Add Concrete Repository Class

---
+++ CommandsService/Data/Repository/ICommandRepo.cs
@@ -0,0 +1,19 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data.Repository
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlaformExits(int platformId);
+
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
