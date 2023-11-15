afdea5791a3fcfa9ab7ed06b50e34a43c2474fe8
Дмитрий Горбунов <bous07@mail.ru>
2023-11-16 01:14:01 +0800

    Event Processor : DetermineEvent and ProcessEvent

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,5 +1,6 @@
 using CommandsService.Data;
 using CommandsService.Data.Repository;
+using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMe
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddEndpointsApiExplorer();
