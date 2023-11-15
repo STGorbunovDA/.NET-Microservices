62fd650c9cb98643413a3de4d18b8a9b5f71046c
Дмитрий Горбунов <bous07@mail.ru>
2023-11-15 20:41:07 +0800

    Automapper Profiles

---
+++ CommandsService/ProfilesAutoMapper/CommandsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+
+namespace CommandsService.ProfilesAutoMapper
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<Command, CommandReadDto>();
+
+        }
+    }
+}
